702ef232a8b72db52a748de4bf13d00a83cff1e3
adburke <adburke@fullsail.edu>
2013-06-17 20:29:13 -0500

    Xcode Project Creation

---
+++ testApp/testApp/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  testApp
+//
+//  Created by Aaron Burke on 6/17/13.
+//  Copyright (c) 2013 Aaron Burke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ testApp/testApp/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  testApp
+//
+//  Created by Aaron Burke on 6/17/13.
+//  Copyright (c) 2013 Aaron Burke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
