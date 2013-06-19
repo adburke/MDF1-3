3278ef803ace877af1eff9a9d576c0f55a025d31
adburke <adburke@fullsail.edu>
2013-06-18 19:34:47 -0500

    Setting up Navigation Controller for FirstViewController along with creating the detail view

--- testApp/testApp/FirstViewController.h
+++ testApp/testApp/FirstViewController.h
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FirstViewController : UIViewController
+@interface FirstViewController : UIViewController <UITableViewDelegate>
+{
+    IBOutlet UITableView *mainTable;
+}
 
 @end
---
+++ testApp/testApp/MoreInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoreInfoViewController.h
+//  testApp
+//
+//  Created by Aaron Burke on 6/18/13.
+//  Copyright (c) 2013 Aaron Burke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreInfoViewController : UIViewController
+
+@end
