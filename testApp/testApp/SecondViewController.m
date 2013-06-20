b6bf6f0dfe0565087cb87c8c8a27764d4a19acc6
adburke <adburke@fullsail.edu>
2013-06-20 13:14:40 -0500

    Corrected annotations not refreshing by removing them and then re-adding them on load

--- testApp/testApp/SecondViewController.m
+++ testApp/testApp/SecondViewController.m
@@ -27,11 +27,43 @@
 							
 - (void)viewDidLoad
 {
-    self.locationShare = [LocationsManager sharedInstance];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
     
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    // Removes annotations so they can be refreshed on new load
+    [self.mapView removeAnnotations:self.mapView.annotations];
+    
+    // Pull current instance of location manager singleton
+    self.locationShare = [LocationsManager sharedInstance];
+    //NSLog(@"%@", self.locationShare.locationsArray);
+    
+    NSMutableArray *locationHolder = [[NSMutableArray alloc]init];
+    
+    // Iterate through singleton to pull out location data - used fast enumeration on array and block on dictionary
+    for (id obj1 in self.locationShare.locationsArray){
+        //NSLog(@"obj=%@", obj1);
+        [obj1 enumerateKeysAndObjectsUsingBlock:^(id key, id obj2, BOOL *stop) {
+            //NSLog(@"%@ => %@", key, obj2);
+            if ([key isEqualToString:@"Loc"]) {
+                CLLocationCoordinate2D location;
+                location.latitude = [obj2[0] doubleValue];
+                location.longitude = [obj2[1] doubleValue];
+                
+                // Gather annotations in array
+                MapAnnotation *annotation = [[MapAnnotation alloc] initWithInfo:obj1[@"Hotel"] subText:@"Test" coord:location];
+                if (annotation) {
+                    [locationHolder addObject:annotation];
+                }
+            }
+        }];
+    }
+    // Load the array of annotations
+    [self.mapView addAnnotations:locationHolder];
 }
 
 - (void)didReceiveMemoryWarning
