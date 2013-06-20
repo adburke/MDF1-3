ebf6d5c986ee2463d62ffe743700a36e6f4c55ad
adburke <adburke@fullsail.edu>
2013-06-20 11:21:01 -0500

    Added more hotel data to meet the requirement of 10

--- testApp/testApp/LocationsManager.m
+++ testApp/testApp/LocationsManager.m
@@ -33,6 +33,12 @@
                           @{@"Hotel":@"The Bostonian", @"City":@"Boston", @"State":@"Massachusetts", @"Loc":@[@42.3583,@-71.0603], @"Image":@"the-bostonian.jpg", @"Pictures":@[@"pic-1.jpg",@"pic-2.jpg",@"pic-3.jpg",@"pic-4.jpg",@"pic-5.jpg"]},
                           @{@"Hotel":@"Tropicana", @"City":@"Cabo San Lucas", @"State":@"Mexico", @"Loc":@[@22.8800,@-109.9000], @"Image":@"tropicana.jpg", @"Pictures":@[@"pic-1.jpg",@"pic-2.jpg",@"pic-3.jpg",@"pic-4.jpg",@"pic-5.jpg"]},
                           @{@"Hotel":@"NewYorker", @"City":@"New York City", @"State":@"New York", @"Loc":@[@40.7142,@-74.0064], @"Image":@"newyorker.jpg", @"Pictures":@[@"pic-1.jpg",@"pic-2.jpg",@"pic-3.jpg",@"pic-4.jpg",@"pic-5.jpg"]},
+                          @{@"Hotel":@"Orlandian", @"City":@"Orlando", @"State":@"Florida", @"Loc":@[@28.5381,@-81.3794], @"Image":@"orlandian.jpg", @"Pictures":@[@"pic-1.jpg",@"pic-2.jpg",@"pic-3.jpg",@"pic-4.jpg",@"pic-5.jpg"]},
+                          @{@"Hotel":@"Nashvillage", @"City":@"Nashville", @"State":@"Tennessee", @"Loc":@[@36.1658,@-86.7844], @"Image":@"nashvillage.jpg", @"Pictures":@[@"pic-1.jpg",@"pic-2.jpg",@"pic-3.jpg",@"pic-4.jpg",@"pic-5.jpg"]},
+                          @{@"Hotel":@"Los Ango", @"City":@"Los Angelos", @"State":@"California", @"Loc":@[@34.05222,@-118.2428], @"Image":@"los-ango.jpg", @"Pictures":@[@"pic-1.jpg",@"pic-2.jpg",@"pic-3.jpg",@"pic-4.jpg",@"pic-5.jpg"]},
+                          @{@"Hotel":@"The Dallaso", @"City":@"Dallas", @"State":@"Texas", @"Loc":@[@32.7828,@-96.8039], @"Image":@"the-dallaso.jpg", @"Pictures":@[@"pic-1.jpg",@"pic-2.jpg",@"pic-3.jpg",@"pic-4.jpg",@"pic-5.jpg"]},
+                          @{@"Hotel":@"Cozume", @"City":@"Cozumel", @"State":@"Mexico", @"Loc":@[@20.5000,@-86.9500], @"Image":@"cozume.jpg", @"Pictures":@[@"pic-1.jpg",@"pic-2.jpg",@"pic-3.jpg",@"pic-4.jpg",@"pic-5.jpg"]},
+                          @{@"Hotel":@"Chitown", @"City":@"Chicago", @"State":@"Illinois", @"Loc":@[@41.8500,@-87.6500], @"Image":@"chitown.jpg", @"Pictures":@[@"pic-1.jpg",@"pic-2.jpg",@"pic-3.jpg",@"pic-4.jpg",@"pic-5.jpg"]},
                           nil];
     }
     return self;
